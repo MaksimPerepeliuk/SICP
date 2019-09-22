5aa64e9d5b9cc6af2abf3bb57c99f03863ed7e91
Max Perepelyuk <maksim.perepelyuk@mail.ru>
2019-09-23 01:07:33 +0300

    update e 1.17, 1,18

--- chapters/chapter1/e1-17.rkt
+++ chapters/chapter1/e1-17.rkt
@@ -1,6 +1,7 @@
 #lang sicp
-(define (fast-* a b) 
+
+(define (fast-mul a b) 
   (cond ((= b 0) 0) 
         ((= b 1) a) 
-        ((even? b) (double (fast-* a (halve b)))) 
-        (else (+ a (fast-* a (- b 1))))))
+        ((even? b) (square (fast-mul a (halve b)))) 
+        (else (+ a (fast-mul a (- b 1))))))
--- chapters/chapter1/e1-18.rkt
+++ chapters/chapter1/e1-18.rkt
@@ -1,8 +1,9 @@
 #lang sicp
 
-(define (fast-* a b) 
-  (fast-*-iter a b 0))
-(define (fast-*-iter a b c) 
+(define (fast-mul2 a b) 
+  (fast-mul-iter a b 0))
+(define (fast-mul-iter a b c) 
   (cond ((= b 0) c) 
-        ((even? b) (fast-*-iter (double a) (halve b) c)) 
-        (else (fast-*-iter a (- b 1) (+ c a)))))
+        ((even? b) (fast-mul-iter (square a) (halve b) c)) 
+        (else (fast-mul-iter a (- b 1) (+ c a)))))
+
