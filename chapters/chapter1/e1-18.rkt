1f5e92cd000c020845b9b73ec88566678bc145af
Max Perepelyuk <kostin7778@gmail.com>
2019-09-22 20:05:37 +0300

    added solution for ex 1.18

---
+++ chapters/chapter1/e1-18.rkt
@@ -0,0 +1,6 @@
+(define (fast-* a b) 
+  (fast-*-iter a b 0))
+(define (fast-*-iter a b c) 
+  (cond ((= b 0) c) 
+        ((even? b) (fast-*-iter (double a) (halve b) c)) 
+        (else (fast-*-iter a (- b 1) (+ c a)))))
